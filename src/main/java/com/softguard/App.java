0e0baab7025d40b9d0e1a2a81b92994877b6540b
User <User@192.168.100.138>
2025-04-28 20:15:48 -0300

    inicializar repositório e projeto base

---
+++ src/main/java/com/softguard/App.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.softguard;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao SoftGuard - Sistema de Gestão de Licenças e Equipamentos de TI!");
+    }
+}
